feb752b27cb3c65a84357fdf47a0976a1cf3ac2d
vamsi krishna mutyala <vamsi.mutyala@zink.co.in>
2018-01-22 18:39:32 +0530

    added new route for task

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Task 1' });
 });
 
+router.get('/task', function(req, res, next) {
+  res.render('index', { title: 'Task 3' });
+});
+
 module.exports = router;
